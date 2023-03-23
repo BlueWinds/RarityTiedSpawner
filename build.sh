a4f6e6a62d2870fe9b8620b103d5e8125bb462c1
BlueWinds <blue@everblue.info>
2023-03-23 08:29:47 -0700

    Update to modern csproj format

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -o errexit
 
-export BTPATH=../BATTLETECH/game
-dir=$(pwd)
+BTPATH=/home/blue/Games/BATTLETECH/game
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -10,8 +9,8 @@ NC='\033[0m' # No Color
 if ! command -v nodemon &> /dev/null
 then
     echo -e "${RED}nodemon could not be found, compiling once and exiting.${NC}"
-    msbuild
+    ~/.dotnet/dotnet build --verbosity normal --configuration Release -p:BattleTechGameDir='$BTPATH'
     exit
 fi
 
-nodemon -x "msbuild && rm -f 'RarityTiedSpawner.zip' && cd '$BTPATH/Mods/RarityTiedSpawner' && zip -x RarityTiedSpawner.log -x WIIC_systemControl.json -rq '$PWD/RarityTiedSpawner.zip' ." -w src/ -e .
+nodemon -x "~/.dotnet/dotnet build --verbosity normal --configuration Release -p:BattleTechGameDir='$BTPATH' && rm -f 'RarityTiedSpawner.zip' && cd '$BTPATH/Mods/RarityTiedSpawner' && zip -x RarityTiedSpawner.log -rq '$PWD/RarityTiedSpawner.zip' ." -w src/ -e .
